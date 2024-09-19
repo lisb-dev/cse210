ed3d7e62d56607f03d07227c8f8ae15e9b9a953f
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2024-09-19 19:17:04 -0300

    feat: complete fourth activity

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+      Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+      List<int> numbers = new List<int>();
+
+      int number = 1;
+      while (number != 0)
+      {
+        Console.Write("Enter number: ");
+        number = int.Parse(Console.ReadLine());
+        if (number != 0)
+        {
+          numbers.Add(number);
+        }
+      }
+
+      int sum = numbers.Sum();
+      Console.WriteLine($"The sum is: {sum}");
+
+      double average = numbers.Average();
+      Console.WriteLine($"The average is: {average}");
+
+      int max = numbers.Max();
+      Console.WriteLine($"The max is: {max}");
+
+
+
     }
 }
