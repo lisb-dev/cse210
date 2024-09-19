6f97b74feb86dc4dde5fdb60eeef080ee902bb1c
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2024-09-19 19:09:00 -0300

    feat: complete third activity

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+      var magicNumber = new Random().Next(1, 100);
+      Console.WriteLine("Guess a number between 1 and 100:");
+      int guess = Convert.ToInt32(Console.ReadLine());
+      while (guess != magicNumber)
+      {
+        if (guess > magicNumber)
+        {
+          Console.WriteLine("Lower");
+        }
+        else
+        {
+          Console.WriteLine("Higher");
+        }
+        guess = Convert.ToInt32(Console.ReadLine());
+      }
+      Console.WriteLine("You guessed it!");
     }
 }
