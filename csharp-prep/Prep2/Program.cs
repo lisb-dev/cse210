1b5f6f55a3d24b500197f0a7a40becc794704dae
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2024-09-19 19:05:18 -0300

    feat: complete second activity

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+      string letter;
+      Console.WriteLine("What is your grade?");
+      var grade = Convert.ToInt32(Console.ReadLine());
+
+      if (grade >= 90)
+      {
+        letter = "A";
+      } else if (grade >= 80)
+      {
+        letter = "B";
+      } else if (grade >= 70)
+      {
+        letter = "C";
+      } else if (grade >= 60)
+      {
+        letter = "D";
+      } else
+      {
+        letter = "F";
+      }
+
+      Console.WriteLine($"Your grade is {letter}");
+
+      if (grade >= 70)
+      {
+        Console.WriteLine("Props mate!");
+      } else
+      {
+        Console.WriteLine("Better luck next time!");
+      }
     }
 }
